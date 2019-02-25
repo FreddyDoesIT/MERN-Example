bda91963f1620266d0e970feb04c9f3f6f7ebfe9
freddy <zhonglyu173@gmail.com>
2019-02-25 10:29:22 -0800

    serving react from express

--- front/src/App.js
+++ front/src/App.js
@@ -1,9 +1,28 @@
 import React, {Component} from "react";
 import "./index.css";
+
+
 class App extends React.Component{
+
+  componentDidMount() {
+    fetch("/api/getMessages")
+      .then(res => console.log("got data?", res));
+  }
+
   render() {
     console.log("Rendering");
-    return <div className = "App"> Hola FFF!</div>;
+    return (
+      <div className="App">
+
+        <h1>Comments!</h1>
+
+        <div id="comments"></div>
+        <form action="/api/createMessage">
+          <input type="text" id="comment"/>
+        </form>
+        <div>Made by Freddy with Respect to John <span role="img">♥️</span></div>
+      </div>
+      ) 
   }
   
 }
--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,15 @@ var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get("/", function(req, res, next) {
-  res.render("index", { title: "Express" });
+router.post("/createMessage", function(req, res, next) {
+  res.send("Hola");
+});
+
+router.get("/getMessages", function(req, res, next) {
+	console.log("get Messages!!!!!");
+  res.send([
+  		{text:"Freddy"}
+  	]);
 });
 
 module.exports = router;
